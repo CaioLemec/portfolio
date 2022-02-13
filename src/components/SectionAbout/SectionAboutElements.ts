951b392346bfd3d89ec525c231a296874a5809fe
Caio Lemec <caio.lemec@gmail.com>
2022-02-13 16:47:27 -0300

    addind resume download buttons

--- src/components/SectionAbout/SectionAboutElements.ts
+++ src/components/SectionAbout/SectionAboutElements.ts
@@ -46,23 +46,78 @@ export const DescriptionAboutWrapper = styled.div`
 
 `;
 
+export const DescriptionTitle = styled.div`
+  color: ${(props) => props.theme.colors.fontColor};
+  text-align: center;
+  font-size: 1.5rem;
+  text-align: left;
+  font-weight: bold;
+  margin-bottom: 2rem;
+
+  @media screen and (max-width: 1173px) {
+    font-size: 1.2rem;
+  }
+
+  @media screen and (max-width: 500px) {
+    font-size: 1rem;
+  }
+
+  @media screen and (max-width: 375px) {
+    font-size: 0.9rem;
+  }
+`;
+
 export const Description = styled.div`
   color: ${(props) => props.theme.colors.fontColor};
   text-align: center;
   font-size: 1rem;
   text-align: left;
   font-weight: bold;
+  margin-bottom: 2rem;
 
   @media screen and (max-width: 1173px) {
-    font-size: 3rem;
+    font-size: 0.8rem;
   }
 
   @media screen and (max-width: 500px) {
-    font-size: 2rem;
+    font-size: 0.6rem;
   }
 
   @media screen and (max-width: 375px) {
-    font-size: 1rem;
+    font-size: 0.5rem;
   }
 `;
+
+export const WrapperResumeBtn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+`;
+
+export const ResumeBtn = styled.a`
+  width: 100%;
+  text-decoration: none;
+  display: flex;
+  font-weight: bold;
+  font-size: 1.025rem;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border: 0;
+  height: 3rem;
+  border-radius: 0.25rem;
+  color: ${(props) => props.theme.colors.contrast};
+  background: ${(props) => props.theme.colors.highLight};
+  transition: 0.2s;
+
+  &:last-child {
+    margin-left: 2rem;
+  }
+
+  &:hover {   
+    filter: brightness(0.8);
+  }
+`;
+
 
--- src/components/SectionAbout/index.tsx
+++ src/components/SectionAbout/index.tsx
@@ -1,24 +1,52 @@
-
 import {
   AboutContainer,
   MaxWidthLimitWrapper,
   AboutContent,
   DescriptionAboutWrapper,
+  DescriptionTitle,
   Description,
+  WrapperResumeBtn,
+  ResumeBtn,
 } from "./SectionAboutElements";
 import SectionAboutMap from "./SectionAboutMap";
 
-const SectionAbout = () => {
+const downloadResumeBR =
+  "https://drive.google.com/uc?export=download&id=1nqS2rf9Azpp1o_2L8wPTQ5R2rlbJmUXW";
+const downloadResumeUS =
+  "https://drive.google.com/uc?export=download&id=1nqS2rf9Azpp1o_2L8wPTQ5R2rlbJmUXW";
 
+const SectionAbout = () => {
   return (
     <AboutContainer id="about">
       <MaxWidthLimitWrapper>
-          <AboutContent>
+        <AboutContent>
           <SectionAboutMap />
           <DescriptionAboutWrapper>
-          <Description>My name is Caio Lemec and I am a passionate self-taught front-end developer. I'm from Brazil, living in Rio de Janeiro and currently work as an intern in software developer. I started my career in engineering and marketing, but found in programming the passion I was looking for move forward.</Description>
+            <DescriptionTitle>About:</DescriptionTitle>
+            <Description>
+              My name is Caio Lemec and I am a passionate self-taught developer.
+              I'm from Brazil, living in Rio de Janeiro and currently work as an
+              intern in software developer. I started my career in engineering
+              and marketing, but found in programming the passion I was looking
+              for move forward.
+            </Description>
+            <DescriptionTitle>Resume:</DescriptionTitle>
+            <WrapperResumeBtn>
+              <ResumeBtn
+                href={downloadResumeBR}
+                target="_blank"
+                rel="noopener noreferrer"
+                download
+              >en-US</ResumeBtn>
+              <ResumeBtn
+                href={downloadResumeUS}
+                target="_blank"
+                rel="noopener noreferrer"
+                download
+              >pt-BR</ResumeBtn>
+            </WrapperResumeBtn>
           </DescriptionAboutWrapper>
-          </AboutContent>
+        </AboutContent>
       </MaxWidthLimitWrapper>
     </AboutContainer>
   );
